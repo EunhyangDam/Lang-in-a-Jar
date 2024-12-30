f282c3c632c36ed7eeb036faab2058847764124e
eunhyangDAM <javerlialee@gmail.com>
2024-12-30 20:20:24 +0900

    BG 자동화 수정

--- js/bg.js
+++ js/bg.js
@@ -1,3 +1,3 @@
 const bgimg = document.querySelector(".session_card");
 const bgImgSrc = bgimg.getAttribute("src");
-document.body.style.backgroundImage = "url(../" + bgImgSrc + ")";
+document.body.style.backgroundImage = "url(" + bgImgSrc + ")";
